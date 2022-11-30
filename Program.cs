a022ce62e0382c3ec9b0f5481f4b8090e91269e9
Luckkyf3r <39806781+BogdanRulea@users.noreply.github.com>
2022-11-30 13:38:52 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    private static string convertBase10ToBase2(string base10)
+    {
+        string answer = "";
+        int number = int.Parse(base10);
+
+        while(number > 0)
+        {
+            answer = number % 2 + answer;
+            number /= 2;
+        }
+
+        return answer;
+    }
+    private static string binaryClock(string currentTime)
+    {
+
+        string[] currentTimeArray = currentTime.Split(':');
+        string output = "";
+
+        // convert hours in binary format
+        string hourBinary = convertBase10ToBase2(currentTimeArray[0]);
+        
+        // convert minutes in binary format
+        string minuteBinary = convertBase10ToBase2(currentTimeArray[1]);
+
+        // convert seconds in binary format
+        string secondBinary = convertBase10ToBase2(currentTimeArray[2]);
+
+        output = hourBinary + ":" + minuteBinary + ":" + secondBinary;
+
+        return output;
+    }
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(binaryClock(DateTime.Now.ToString("HH:mm:ss")));
+    }
+}
